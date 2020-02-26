1e9109c28664da8d999c459344e8b5e023d3164a
mohamed.karam <mohamed.karam@z2data.com>
2020-02-26 14:12:34 +0200

    2.2

--- projects/blazor-ss/build.bat
+++
@@ -1,7 +0,0 @@
-dotnet build HelloWorld
-dotnet build JsIntegration
-dotnet build DependencyInjection
-dotnet build Layout
-dotnet build RssReader
-dotnet build RssReader-2
-dotnet build StartingVariation
